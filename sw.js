3c1597b048b1660d82009bb7187c04c40ae55d0c
sarin <sarin.c.jacob2001@gmail.com>
2026-02-28 13:10:22 +0530

    pwa support

--- sw.js
+++ sw.js
@@ -1,7 +1,42 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
+const CACHE_NAME = 'inky-hub-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './app.js',
+    './manifest.json',
+    './icon.svg'
+];
+
+// Install the service worker and cache the core assets
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
 });
 
-self.addEventListener('fetch', (e) => {
-    // Basic passthrough, you can add caching here later if you want offline use
+// Intercept fetch requests and serve from cache if offline
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            // Return cached response if found, otherwise fetch from network
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
+
+// Clean up old caches when a new service worker takes over
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
 });
